efa2eafc7b7974f856a7f60ec7de20d6d9828d8f
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-19 21:52:35 +0200

    jwt strategy and local strategy added w/ packages.

---
+++ src/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const { secret } = require('./config');
+const User = require('./api/resources/user/user.model');
+const LocalStrategy = require('passport-local').Strategy;
+
+// jwt strategy
+const f = async (payload, done) => {
+  try {
+    // find user from token
+    const user = await User.findById(payload.sub);
+    // handle if user doesn't exist'
+    if (!user) {
+      return done(null, false);
+    }
+    // return user
+    done(null, user);
+  } catch (e) {
+    done(e, false);
+  }
+};
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: secret
+}, f));
+
+// local strategy
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  async (email, password, done) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return done(null, false);
+    }
+  }));
