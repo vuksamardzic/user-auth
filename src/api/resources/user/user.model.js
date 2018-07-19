7416b8752ee40bee1c2f415465854a21cb8ba219
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-19 22:14:13 +0200

    pre save on User model, salt and hash password.

--- src/api/resources/user/user.model.js
+++ src/api/resources/user/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const schema = {
   name: {
@@ -18,5 +20,17 @@ const schema = {
 };
 
 const userSchema = mongoose.Schema(schema);
+
+userSchema.pre('save', async function (next) {
+  try {
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // generate pw hash (salt + hash) and assign for inserting in db
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = User = mongoose.model('user', userSchema);
