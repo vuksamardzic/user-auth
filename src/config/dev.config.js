fba70dcbe3b305fea57801481b092867c0f27681
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-19 21:33:32 +0200

    secret added to config dev and prod.

--- src/config/dev.config.js
+++ src/config/dev.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: {
     url: 'mongodb://localhost:27017/user-auth'
-  }
+  },
+  secret: 'vuk12345'
 };
--- src/config/prod.config.js
+++ src/config/prod.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: {
     url: 'mongodb://v:e1392781@ds111804.mlab.com:11804/user-auth'
-  }
+  },
+  secret: 'e1392781243'
 };
