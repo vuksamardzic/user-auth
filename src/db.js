514c58d1fad451172f424fcd53cde9126d983d14
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-11-04 17:20:59 +0100

    chore: dependency, db update

--- src/db.js
+++ src/db.js
@@ -5,7 +5,12 @@ const { db } = require('./config');
 mongoose.Promise = global.Promise;
 
 const connect = async () => {
-  const conn = await mongoose.connect(db.url, { useNewUrlParser: true });
+  const options = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+  const conn = await mongoose.connect(db.url, options);
   if (conn) {
     console.log(`db connected on port #${conn.connections[0].port}`);
   }
